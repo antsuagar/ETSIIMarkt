caf4597594eeb8566f823f2d452a4a9510ae451a
Antonio José Suárez García <antsuagar@alum.us.es>
2023-11-20 18:28:39 +0100

    Algunos archivos sobrantes borrados

--- EtsiiMarktProject/productos/views.py
+++ EtsiiMarktProject/productos/views.py
@@ -1,21 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-<<<<<<< HEAD
 from .models import Producto
 
 #Catalogo de Productos
 def catalogo(request):
     productos = Producto.objects.all()
     return render(request, 'catalogo.html', {'productos': productos})
-<<<<<<< HEAD
-=======
 
-
-def index(request):
-    contexto = {'productos':'Incluir listado de productos'}
-    
-    return render(request, 'productos.html', contexto)
->>>>>>> a316c19f ([dev] Vista base y de productos)
-=======
-
->>>>>>> 7a6efd02d3ba70b9476f1f03c7959ec1114785d6
