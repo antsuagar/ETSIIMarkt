0231228e046f1c76dab0fb6139fcec49084dfc4f
Antonio José Suárez García <antsuagar@alum.us.es>
2023-11-20 18:52:19 +0100

    kk

--- EtsiiMarktProject/productos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Categoria, Fabricante, Producto
-
-admin.site.register(Producto)
-admin.site.register(Categoria)
-admin.site.register(Fabricante)
--- EtsiiMarktProject/productos/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Producto
-
-#Catalogo de Productos
-def catalogo(request):
-    productos = Producto.objects.all()
-    return render(request, 'catalogo.html', {'productos': productos})
-
