3610f80bf179d0d2283bcb5fedc3b3a173594f25
isma13 <isma13@gmail.com>
2023-11-30 16:13:27 +0100

    [fix] Revisión testing

--- EtsiiMarktProject/core/views.py
+++ EtsiiMarktProject/core/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from productos.models import Producto
 
 def index(request):    
     user = request.user
-    return render(request, 'home.html', {'user': user})
+    productos = Producto.objects.all()
+    return render(request, 'home.html', {'user': user, 'productos': productos})
+    
