c82a43ccb4f3d61e2fcb70f87de83d26fc93789d
diemargon4 <diego_16_rey@hotmail.com>
2023-12-05 13:43:44 +0100

    Comienzo de la tarea

--- EtsiiMarktProject/pedidos/views.py
+++ EtsiiMarktProject/pedidos/views.py
@@ -1,8 +1,9 @@
 import uuid
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import Pedido, ProductoPedido
 from productos.models import Fabricante, Producto, Categoria
+from datetime import datetime 
 
 # Create your views here.
 def carrito(request):
@@ -19,15 +20,11 @@ def carrito(request):
             nuevo_pedido.save()
             items = []
             pedido = {'get_total_carrito':0, 'get_productos_carrito':0}
-                
-               
+       
         else:
             anonimo_id = request.session['anonimo_id']
             pedido = get_object_or_404(Pedido, id_transaccion=anonimo_id)
             items = pedido.get_lista_de_productos_carrito()
-            
-          
-        
 
     context={'items': items, 'pedido': pedido}
     return render(request, 'productos/carrito.html', context)
@@ -43,9 +40,7 @@ def actualizar(request, producto_id):
     else:
         anonimo_id = request.session['anonimo_id']
         pedido = get_object_or_404(Pedido, id_transaccion=anonimo_id)
-        
-        
-            
+
     producto_pedido, createdP= ProductoPedido.objects.get_or_create(pedido=pedido,producto=q_producto)
     producto_pedido.cantidad = q_cantidad
     producto_pedido.save()   
@@ -55,9 +50,6 @@ def actualizar(request, producto_id):
     context={'items': items, 'pedido': pedido}
     return render(request, 'productos/carrito.html', context)
 
-
-
-    
 
 def eliminar(request, producto_id):
     q_producto = get_object_or_404(Producto, pk=producto_id)
@@ -79,3 +71,33 @@ def eliminar(request, producto_id):
 
     context={'items': items, 'pedido': pedido}
     return render(request, 'productos/carrito.html', context)
+
+
+def formulario_envio(request, id_transaccion):
+    # Lógica para manejar el formulario de envío
+    return render(request, 'formulario_envio.html', {'id_transaccion': id_transaccion})
+
+def procesar_pedido(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        email = request.POST.get('email')
+        direccion = request.POST.get('direccion')
+        transaccion_id = request.POST.get('transaccion_id')
+        completado = request.POST.get('completado')
+
+        # Obtiene la fecha y hora actuales
+        fecha_pedido = datetime.now()
+
+        # Crea un nuevo objeto Pedido con los datos del formulario y la fecha actual
+        pedido = Pedido.objects.create(
+            
+            fecha_pedido=fecha_pedido,
+            completado=completado,
+            transaccion_id=transaccion_id,
+           
+            # Otros campos del pedido
+        )
+
+        # Lógica adicional, como enviar correos electrónicos, actualizar inventarios, etc.
+
+        return redirect('home')
