cce55950d143489bb0122fce244e55b1124eb0b9
isma13 <isma13@gmail.com>
2023-12-11 08:57:17 +0100

    [fix] Correcciones home

--- EtsiiMarktProject/core/views.py
+++ EtsiiMarktProject/core/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from productos.models import Producto
+from productos.models import Producto, Categoria
 
 def index(request):    
     user = request.user
     productos = Producto.objects.all()
-    return render(request, 'home.html', {'user': user, 'productos': productos})
+    categorias = Categoria.objects.all()
+    return render(request, 'home.html', {'user': user, 'productos': productos, 'categorias': categorias})
     
